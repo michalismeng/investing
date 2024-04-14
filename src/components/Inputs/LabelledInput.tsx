554b1301e1b6815f1d638174f4da464dd901bf4b
Michalis Mengisoglou <michalis.mengisoglou@gmail.com>
2024-04-14 20:42:21 +0300

    Add LabelledInput component

---
+++ src/components/Inputs/LabelledInput.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+  divider?: boolean;
+}
+
+const LabelledInput = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ label, type = "text", divider = true, ...props }, ref) => {
+    return (
+      <label className="input input-bordered flex items-center">
+        <div className={type == "checkbox" ? "order-last ms-4" : ""}>{label}</div>
+        {divider && (
+          <div className="divider divider-horizontal"></div>
+        )}
+        <input type={type} aria-label={label} ref={ref} {...props} />
+      </label>
+    );
+  }
+);
+
+export default LabelledInput;
