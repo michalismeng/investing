f7393595c9a18bc4e4201f2452570770583f629f
Michalis Mengisoglou <michalis.mengisoglou@gmail.com>
2024-04-20 23:14:09 +0300

    Add some tooltips

--- src/components/CompanyComponent.tsx
+++ src/components/CompanyComponent.tsx
@@ -16,17 +16,17 @@ export const CompanyInTableComponent = ({company: company}: { company: CompanyWi
         <div className="flex flex-row justify-start items-center select-none">
           {(company.ddmValuations.length > 0 && (
             <>
-              <div className="w-36 flex flex-row gap-2 justify-start items-center">
+              <div className="w-36 flex flex-row gap-2 justify-start items-center tooltip" data-tip="Date of the valuation">
                 <CalendarIcon className="h-6 w-6" />
                 {new Date(company.ddmValuations[0].date).toDateString()}
               </div>
               <div className="divider divider-horizontal"></div>
-              <div className="w-28 flex flex-row gap-2 justify-start items-center">
+              <div className="w-28 flex flex-row gap-2 justify-start items-center tooltip" data-tip="Report that the numbers of the valuation are based on">
                 <ClipboardDocumentListIcon className="h-6 w-6" />
                 {company.ddmValuations[0].referenceReport}
               </div>
               <div className="divider divider-horizontal"></div>
-              <div className="flex flex-row gap-2 justify-start items-center">
+              <div className="flex flex-row gap-2 justify-start items-center tooltip" data-tip="Value of the stock">
                 <CurrencyDollarIcon className="h-6 w-6" />
                 {currencyFormatter(company.ddmValuations[0].pvStock)}
               </div>
