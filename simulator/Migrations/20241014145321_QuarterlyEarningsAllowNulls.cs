5379a0f9e339877dfc732519b8d24569bb03668f
Michalis Mengisoglou <michalis.mengisoglou@gmail.com>
2024-10-14 17:54:29 +0300

    Allow quarterly earnings null values

---
+++ simulator/Migrations/20241014145321_QuarterlyEarningsAllowNulls.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace simulator.Migrations
+{
+    /// <inheritdoc />
+    public partial class QuarterlyEarningsAllowNulls : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Surpirse",
+                table: "QuarterlyEarnings",
+                type: "decimal(65,30)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(65,30)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "ReportedEPS",
+                table: "QuarterlyEarnings",
+                type: "decimal(65,30)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(65,30)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Surpirse",
+                table: "QuarterlyEarnings",
+                type: "decimal(65,30)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(65,30)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "ReportedEPS",
+                table: "QuarterlyEarnings",
+                type: "decimal(65,30)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(65,30)",
+                oldNullable: true);
+        }
+    }
+}
--- simulator/Model/QuarterlyEarningsEntry.cs
+++ simulator/Model/QuarterlyEarningsEntry.cs
@@ -7,12 +7,12 @@ public class QuarterlyEarningsEntry
     public required string Ticker { get; set; }
     public DateTime FiscalDateEnding { get; set; }
     public DateTime ReportedDate { get; set; }
-    public decimal ReportedEPS { get; set; }
-    public decimal Surpirse { get; set; }
+    public decimal? ReportedEPS { get; set; }
+    public decimal? Surpirse { get; set; }
 
     [NotMapped]
-    public decimal EstimatedEPS  => ReportedEPS - Surpirse;
+    public decimal? EstimatedEPS  => ReportedEPS - Surpirse;
     [NotMapped]
-    public decimal SurprisePercentage  => Surpirse / EstimatedEPS;
+    public decimal? SurprisePercentage  => Surpirse / EstimatedEPS;
 }
 
