df8117b7f4806d8e941a7f3bfbff02cc22ec678f
Michalis Mengisoglou <michalis.mengisoglou@gmail.com>
2024-10-19 16:55:06 +0300

    Extend tests

--- tests/Extensions.cs
+++ tests/Extensions.cs
@@ -10,31 +10,70 @@ public class TestExtensions
         var numbers = new List<decimal> { 1, 2, 4, 8, 16, 32, 64, 128 };
         numbers.Reverse();
         var result = StockPriceExtensions.GetYearOverYearChange(numbers, e => e, 3);
-        var expectedChanges = new List<decimal?> { 7, 7, 7, 7, 7 };
+        var expectedChanges = new List<decimal?> { 7, 7, 7, 7, 7, null, null, null };
         expectedChanges.Should().Equal(result.Select(x => x.change).ToList());
+        result.Select(x => x.entry).Should().Equal(numbers);
 
         numbers = [1, 2, 4];
         numbers.Reverse();
         result = StockPriceExtensions.GetYearOverYearChange(numbers, e => e, 3);
         expectedChanges = [null, null, null];
-        expectedChanges.Should().Equal(result.Select(x => x.change).ToList());
+        result.Select(x => x.change).Should().Equal(expectedChanges);
+        result.Select(x => x.entry).Should().Equal(numbers);
 
         numbers = [1, 2];
         numbers.Reverse();
         result = StockPriceExtensions.GetYearOverYearChange(numbers, e => e, 3);
         expectedChanges = [null, null];
-        expectedChanges.Should().Equal(result.Select(x => x.change).ToList());
+        result.Select(x => x.change).Should().Equal(expectedChanges);
+        result.Select(x => x.entry).Should().Equal(numbers);
 
         numbers = [1, 2, 4, 8];
         numbers.Reverse();
         result = StockPriceExtensions.GetYearOverYearChange(numbers, e => e, 3);
-        expectedChanges = [ 7, null, null ];
-        expectedChanges.Should().Equal(result.Select(x => x.change).ToList());
+        expectedChanges = [ 7, null, null, null ];
+        result.Select(x => x.change).Should().Equal(expectedChanges);
+        result.Select(x => x.entry).Should().Equal(numbers);
 
         numbers = [-1, 1];
         numbers.Reverse();
         result = StockPriceExtensions.GetYearOverYearChange(numbers, e => e, 1);
-        expectedChanges = [ 2 ];
+        expectedChanges = [ 2, null ];
         result.Select(x => x.change).Should().Equal(expectedChanges);
+        result.Select(x => x.entry).Should().Equal(numbers);
+
+        numbers = [];
+        numbers.Reverse();
+        result = StockPriceExtensions.GetYearOverYearChange(numbers, e => e, 1);
+        expectedChanges = [];
+        result.Select(x => x.change).Should().Equal(expectedChanges);
+        result.Select(x => x.entry).Should().Equal(numbers);
+    }
+    [Fact]
+    public void TestSmooth()
+    {
+        var numbers = new List<decimal> { 1, 2, 4, 8, 16, 32, 64, 128 };
+        var result = StockPriceExtensions.Smooth(numbers, e => e);
+        var expectedChanges = new List<decimal?> { 1.5M, 3M, 6M, 12M, 24, 48, 96, null };
+        result.Select(x => x.smooth).Should().Equal(expectedChanges);
+        result.Select(x => x.entry).Should().Equal(numbers);
+
+        numbers = [1, 2];
+        result = StockPriceExtensions.Smooth(numbers, e => e);
+        expectedChanges = [1.5M, null];
+        result.Select(x => x.smooth).Should().Equal(expectedChanges);
+        result.Select(x => x.entry).Should().Equal(numbers);
+
+        numbers = [1];
+        result = StockPriceExtensions.Smooth(numbers, e => e);
+        expectedChanges = [null];
+        result.Select(x => x.smooth).Should().Equal(expectedChanges);
+        result.Select(x => x.entry).Should().Equal(numbers);
+
+        numbers = [];
+        result = StockPriceExtensions.Smooth(numbers, e => e);
+        expectedChanges = [];
+        result.Select(x => x.smooth).Should().Equal(expectedChanges);
+        result.Select(x => x.entry).Should().Equal(numbers);
     }
 }
