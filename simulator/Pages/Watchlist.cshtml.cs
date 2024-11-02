bb6cb8fd023e34550f2579cb746b66dd4076c8ba
Michalis Mengisoglou <michalis.mengisoglou@gmail.com>
2024-11-02 12:23:55 +0200

    Add future prices and change filters

--- simulator/Pages/Watchlist.cshtml.cs
+++ simulator/Pages/Watchlist.cshtml.cs
@@ -1,6 +1,7 @@
 using System.Collections.Concurrent;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using simulator.Extensions;
@@ -13,12 +14,14 @@ public class WatchlistTickerInfoModel
 {
     public TickerInfo Ticker { get; set; }
     public TickerData PriceData { get; set; }
+    public List<TickerData> PriceDataFuture { get; set; } = [];
     public List<(QuarterlyEarningsEntry entry, decimal? change, decimal? smooth)> Earnings { get; set; } = [];
     public List<(AnnualEarningsEntry entry, decimal? change, decimal? smooth)> AnnualEarnings { get; set; } = [];
     public List<(QuarterlyIncomeStatement entry, decimal? change, decimal? smooth)> Income { get; set; } = [];
     public decimal? MarketCap { get; set; }
     public decimal? SharesOutstanding { get; set; }
     public decimal VolumeSMA50 { get; set; }
+    public (DateTime, double?) Volatility { get; set; }
 }
 
 public class WatchlistFiltersModel
@@ -106,8 +109,8 @@ public class WatchlistModel : PageModel
                                                      .ToList();
 
 
-        System.Console.WriteLine("Getting max drop of SPY in the last 5 years...");
-        var maxDropSPY = prices.Where(t => t.Ticker == "SPY").OrderBy(p => p.Date).ToList().CalculateGreatestDropPercentage();
+        System.Console.WriteLine("Getting max drop of SPY in the last 2 years...");
+        var maxDropSPY = prices.Where(t => t.Ticker == "SPY" && t.Date >= filters.Date.AddYears(-2)).OrderBy(p => p.Date).ToList().CalculateGreatestDropPercentage();
         System.Console.WriteLine("Max drop of SPY is {0}", maxDropSPY.Round());
 
         System.Console.WriteLine("Applying filters to historical data...");
@@ -116,7 +119,7 @@ public class WatchlistModel : PageModel
             Ticker = g.Key,
             SMA40 = (decimal)g.OrderBy(x => x.Date).GetSma(200).Last().Sma!,
             VolumeSMA50 = (decimal)g.Select(x => new TickerData() { Date = x.Date, Ticker = x.Ticker, Close = x.Volume }).OrderBy(x => x.Date).GetSma(50).Last().Sma!,
-            MaxDrop = g.OrderBy(x => x.Date).ToList().CalculateGreatestDropPercentage(),
+            MaxDrop2Y = g.Where(x => x.Date >= filters.Date.AddYears(-2)).OrderBy(x => x.Date).ToList().CalculateGreatestDropPercentage(),
             PriceData = g.Last(),
             // We need 9 quarters: 8 will give us 4 quarters with yoy change and we need 1 more for the smoothing 
             Earnings = earnings.Where(e => e.Ticker == g.Key).OrderByDescending(e => e.FiscalDateEnding).Take(9).ToList().GetYearOverYearChange().Smooth(),
@@ -124,10 +127,12 @@ public class WatchlistModel : PageModel
             Sales = sales.Where(e => e.Ticker == g.Key).OrderByDescending(e => e.FiscalDateEnding).Take(9).ToList().GetYearOverYearChange().Smooth(),
             MarketCap = balance.Where(e => e.Ticker == g.Key).OrderByDescending(e => e.FiscalDateEnding).FirstOrDefault()?.MarketCap(g.Last().Close),
             SharesOutstanding = balance.Where(e => e.Ticker == g.Key).OrderByDescending(e => e.FiscalDateEnding).FirstOrDefault()?.SharesOutstanding,
+            Volatility = g.OrderBy(x => x.Date).GetStdDev(5).Last(),
         }).Where(p => p.VolumeSMA50 >= filters.Volume &&      // Ensure there is enough volume.
-                      p.Earnings.Count >= 0 && p.Earnings.Take(filters.EarningsGrowthQuarters).All(e => e.smooth.HasValue == false || e.smooth >= filters.EarningsGrowth) &&      // latest 4 quarters should have 20% yoy increase
-                      p.SharesOutstanding.HasValue == false || p.SharesOutstanding <= filters.SharesOutstanding &&
-                      p.MaxDrop <= maxDropSPY + filters.MaxDropOverSPY)
+                      p.Earnings.Count >= 0 && p.Earnings.Where(e => e.change.HasValue).Count() >= filters.EarningsGrowthQuarters &&
+                      p.Earnings.Take(filters.EarningsGrowthQuarters).All(e => e.change.HasValue == false || e.change >= filters.EarningsGrowth) &&      // latest 4 quarters should have 20% yoy increase
+                      ( p.SharesOutstanding <= filters.SharesOutstanding) &&
+                      p.MaxDrop2Y <= maxDropSPY + filters.MaxDropOverSPY)
           .Select(st => new WatchlistTickerInfoModel()
           {
             Ticker = _context.Tickers.FirstOrDefault(x => x.Ticker == st.Ticker)!,
@@ -138,6 +143,16 @@ public class WatchlistModel : PageModel
             MarketCap = st.MarketCap,
             SharesOutstanding = st.SharesOutstanding,
             VolumeSMA50 = st.VolumeSMA50,
+            Volatility = (st.Volatility.Date, st.Volatility.StdDev),
+            PriceDataFuture = [ _context.PriceData.Where(p => p.Ticker == st.Ticker && p.Date >= st.PriceData.Date.AddMonths(1))
+                                            .OrderBy(p => p.Date - st.PriceData.Date.AddMonths(1))      // This is our own check
+                                            .Take(1).SingleOrDefault(),
+                                _context.PriceData.Where(p => p.Ticker == st.Ticker && p.Date >= st.PriceData.Date.AddMonths(2))
+                                            .OrderBy(p => p.Date - st.PriceData.Date.AddMonths(2))      // 7-point method suggests 8 weeks as a make-or-break
+                                            .Take(1).SingleOrDefault(),
+                                _context.PriceData.Where(p => p.Ticker == st.Ticker && p.Date >= st.PriceData.Date.AddDays(13 * 7))
+                                            .OrderBy(p => p.Date - st.PriceData.Date.AddDays(13 * 7))   // And 13 weeks is the maximum time span
+                                            .Take(1).SingleOrDefault()]
           }).Where(x => x.Ticker != null && x.PriceData != null).OrderByDescending(x => x.PriceData.RelativeStrength)];
         System.Console.WriteLine("Finished");
 
